e012f861ae0c341844de20fad40081e176cb0563
iMythms <maythams.149@gmail.com>
2024-11-10 01:25:42 +0300

    Fine tuning

--- app.js
+++ app.js
@@ -3,102 +3,117 @@ const mongoose = require('mongoose')
 const Customer = require('./models/Customer')
 const prompt = require('prompt-sync')()
 
-const username = prompt('What is your name? ')
-
-console.log(`Your name is ${username}`)
-
 const connect = async () => {
-	await mongoose.connect(process.env.MONGODB_URI)
-	console.log('Connected to MongoDB')
-
-	await mongoose.disconnect()
-	console.log('Disconnected from MongoDB')
-
-	process.exit()
+	try {
+		await mongoose.connect(process.env.MONGODB_URI)
+		await main()
+		await mongoose.disconnect()
+	} catch (error) {
+		console.error('Error connecting to MongoDB:', error)
+	} finally {
+		process.exit()
+	}
 }
 
-/* Ex1: Developing the user interface
-• Start by displaying a welcome message to the user.
-• Implement a simple menu system that lets the user choose an action (Create, View, Update, Delete, Quit). 
-  Use prompt-sync to get the user’s choice and handle it accordingly.
-• When figuring out what the user wants to do, it’s probably easiest to prompt them to choose from various options in a numbered list. 
-  This way, the user just enters a number and the application knows what to do next.
-• When dealing with choosing a specific customer to update or delete, it’s probably easiest to list the customers in the database 
-  along with their ids. Then prompt the user to enter id of the user that needs to be updated/deleted.
-*/
+const createCustomer = async () => {
+	const customerData = {
+		name: prompt('Enter customer name: '),
+		age: parseInt(prompt('Enter customer age: ')),
+	}
+	const customer = await Customer.create(customerData)
+	console.log(
+		`id: ${customer._id} -- Name: ${customer.name}, Age: ${customer.age}`
+	)
+	console.log('Create Customer successfully')
+}
 
-/* Ex2: Sample exchange
-Use the following example as a guide when designing your application:
+const viewCustomers = async () => {
+	const customers = await Customer.find({})
+	console.log('\nBelow is a list of customers:')
+	customers.forEach((customer) => {
+		console.log(
+			`id: ${customer._id} -- Name: ${customer.name}, Age: ${customer.age}`
+		)
+	})
+}
 
-Starting the application
-Welcome to the CRM
+const updateCustomer = async () => {
+	const customers = await Customer.find({})
+	console.log('\nBelow is a list of customers:')
+	customers.forEach((customer) => {
+		console.log(
+			`id: ${customer._id} -- Name: ${customer.name}, Age: ${customer.age}`
+		)
+	})
+	const id = prompt(
+		'Copy and paste the id of the customer you would like to update here: '
+	)
+	const updatedCustomer = await Customer.findByIdAndUpdate(
+		id,
+		{
+			name: prompt('Enter customer name: '),
+			age: parseInt(prompt('Enter customer age: ')),
+		},
+		{ new: true }
+	)
+	console.log('Updated Customer successfully:', updatedCustomer)
+}
 
-What would you like to do?
+const deleteCustomer = async () => {
+	const customers = await Customer.find({})
+	console.log('\nBelow is a list of customers:')
+	customers.forEach((customer) => {
+		console.log(
+			`id: ${customer._id} -- Name: ${customer.name}, Age: ${customer.age}`
+		)
+	})
+	const id = prompt(
+		'Copy and paste the id of the customer you would like to delete here: '
+	)
+	await Customer.findByIdAndDelete(id)
+	console.log('Deleted Customer successfully')
+}
 
-  1. Create a customer
-  2. View all customers
-  3. Update a customer
-  4. Delete a customer
-  5. quit
+const welcomeMessage = () => {
+	console.log('Welcome to the CRM\n')
+}
 
-Number of action to run: 
-# user inputs 3
-*/
+const showMenu = () => {
+	console.log('\nWhat would you like to do?')
+	console.log('  1. Create a customer')
+	console.log('  2. View all customers')
+	console.log('  3. Update a customer')
+	console.log('  4. Delete a customer')
+	console.log('  5. Quit')
+}
 
-/* Ex3: Updating a customer
-Below is a list of customers: 
+const main = async () => {
+	welcomeMessage()
+	while (true) {
+		showMenu()
+		const choice = parseInt(prompt('Number of action to run: '))
 
-id: 658226acdcbecfe9b99d5421 --  Name: Matt, Age: 43
-id: 65825d1ead6cd90c5c430e24 --  Name: Vivienne, Age: 6
+		switch (choice) {
+			case 1:
+				await createCustomer()
+				break
+			case 2:
+				await viewCustomers()
+				break
+			case 3:
+				await updateCustomer()
+				break
+			case 4:
+				await deleteCustomer()
+				break
+			case 5:
+				console.log('Exiting...')
+				await mongoose.connection.close()
+				return
+			default:
+				console.log('Invalid choice. Please enter a number between 1 and 5.')
+		}
+	}
+}
 
-Copy and paste the id of the customer you would like to update here: 
-# user inputs 658226acdcbecfe9b99d5421
-
-What is the customers new name?
-# user inputs Bilbo
-What is the customers new age?
-# user inputs 50
-*/
-
-/* Ex4: Choosing next action
-What would you like to do?
-
-  1. Create a customer
-  2. View all customers
-  3. Update a customer
-  4. Delete a customer
-  5. Quit
-
-Number of action to run: 
-# user inputs 2
-*/
-
-/* Ex5: Viewing updated customers
-id: 658226acdcbecfe9b99d5421 --  Name: Bilbo, Age: 50
-id: 65825d1ead6cd90c5c430e24 --  Name: Vivienne, Age: 6
-*/
-
-/* Ex6: Choosing next action
-What would you like to do?
-
-  1. Create a customer
-  2. View all customers
-  3. Update a customer
-  4. Delete a customer
-  5. Quit
-
-Number of action to run: 
-# user inputs 5
-*/
-
-/* Note: Exiting the application
-  • When you run your CRM application using node app.js, it starts an active session. 
-    For the application to exit cleanly, it is essential to close the MongoDB connection. 
-    This prevents potential issues like memory leaks or hanging processes.
-
-  • When the exit condition is met (e.g., the user selects ‘Quit’), call mongoose.connection.close() in app.js. 
-    This command safely closes the connection to your MongoDB database.
-*/
-
-// Establishing the connection
 connect()
